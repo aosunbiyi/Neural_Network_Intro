9a04d3bab0e42fab266253cf26f215ac619536d2
Adebayo Osunbiyi <adebayo.osunbiyi.a@gmail.com>
2023-01-26 09:10:36 +0100

    SingleNeural

---
+++ Layers.py
@@ -0,0 +1,23 @@
+
+inputs = [1.0,2.0,3.0,2.5]
+weights = [
+    [0.2,0.8,-0.5,1],
+    [0.5,-0.91,0.26,-0.5],
+    [-0.26,-0.27,0.17,0.87]
+]
+
+biases = [2.0,3.0,0.5]
+
+# Output of current layer
+
+layer_output = []
+
+# For each neuron
+
+for neuron_weights, neuron_bias in zip(weights,biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
+    layer_output.append(neuron_output)
+print(layer_output)
